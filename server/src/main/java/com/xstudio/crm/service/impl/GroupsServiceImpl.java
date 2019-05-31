26849eef57dc5bec14e1d5809695d9d7ea094cda
xiaobiao <huangxb0512@gmail.com>
2019-05-31 20:11:12 +0800

    groups

---
+++ server/src/main/java/com/xstudio/crm/service/impl/GroupsServiceImpl.java
@@ -0,0 +1,42 @@
+package com.xstudio.crm.service.impl;
+
+import com.xstudio.crm.mapper.GroupsMapper;
+import com.xstudio.crm.model.Groups;
+import com.xstudio.crm.service.IGroupsService;
+import com.xstudio.spring.mybatis.paginator.IMybatisPaginatorDao;
+import com.xstudio.spring.mybatis.paginator.MybatisPaginatorServiceImpl;
+import com.xstudio.tool.utils.IdWorker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * service implements for table groups
+ * 
+ * @author mybatis generator
+ * @version Fri May 31 16:34:07 CST 2019
+ */
+@Service
+public class GroupsServiceImpl extends MybatisPaginatorServiceImpl<Groups, Long> implements IGroupsService {
+
+    @Autowired
+    private GroupsMapper groupsMapper;
+
+    @Override
+    public IMybatisPaginatorDao<Groups, Long> getRepositoryDao() {
+        return this.groupsMapper;
+    }
+
+    @Override
+    public void setDefaults(Groups record) {
+        // todo
+        if (record.getItemId() == null) {
+            record.setItemId(IdWorker.getId());
+        }
+    }
+
+    @Override
+    public String getActorId(Groups record) {
+        // todo
+        return null;
+    }
+}
---
+++ server/src/main/java/com/xstudio/rest/GroupsRestController.java
@@ -0,0 +1,21 @@
+package com.xstudio.rest;
+
+import com.xstudio.crm.model.Groups;
+import com.xstudio.crm.service.IGroupsService;
+import com.xstudio.spring.mybatis.rest.AbstractMybatisPaginatorRestController;
+import com.xstudio.tool.service.IAbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("groups")
+public class GroupsRestController extends AbstractMybatisPaginatorRestController<Groups, Long> {
+    @Autowired
+    private IGroupsService groupsService;
+
+    @Override
+    public IAbstractService getService() {
+        return groupsService;
+    }
+}
